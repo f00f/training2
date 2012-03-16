9093ad51533ca295e31c92ab632b69d200db0cd1
f00f <f00f@f00f.de>
2012-03-16 09:05:37 +0100

    support for list of upcoming practices

--- inc/practice.php
+++ inc/practice.php
@@ -62,27 +62,33 @@ class Practice {
 		}
 	}
 
-	// Store data of next practice in DB
+	// Store data of next practices in DB
 	static function storeNext($cid) {
 		global $table;
 
 		$cid = mysql_real_escape_string($cid);
-		$text = serialize(self::$next);
-		$when = self::$next['when'];
-		$date = new DateTime("@{$when}");
-		$pid = $date->format('Y-m-d H:i:s');
-		$q = "REPLACE INTO `{$table}` "
-			. "(`club_id`, `practice_id`, `name`, `text`, `when`, `when_dt`, `status`, `ip`, `host`) "
-			. "VALUES "
-			. "('{$cid}', '{$pid}', 'RESET', '{$text}', '{$when}', '{$pid}', '', '', '')";
-		mysql_query($q);
+		// TODO: create one big query
+		foreach(self::$next as $p) {
+			$text = serialize($p);
+			$when = $p['when'];
+			$date = new DateTime("@{$when}");
+			$pid = $date->format('Y-m-d H:i:s');
+			$q = "REPLACE INTO `{$table}` "
+				. "(`club_id`, `practice_id`, `name`, `text`, `when`, `when_dt`, `status`, `ip`, `host`) "
+				. "VALUES "
+				. "('{$cid}', '{$pid}', 'RESET', '{$text}', '{$when}', '{$pid}', '', '', '')";
+			mysql_query($q);
+		}
 	}
 
+	// find list of practices for next week
 	static function findNext($cid) {
 		if (! self::$next) {
 			global $tag2Int;
 
+			self::$next = array();
 			$trainingDates = array();
+
 			foreach(self::$trainings as $t) {
 				$wdayIdx = $tag2Int[$t['tag']];
 				$trainingDates[$wdayIdx] = $t;
@@ -93,10 +99,14 @@ class Practice {
 			$nextTraining = false;
 			$iter         = 0;
 
-		//	$now = strtotime('+1 hour'); // timezone correction
+			//	$now = strtotime('+1 hour'); // timezone correction
 			$nowTime = date('H:i');
-			while (false === $nextTraining AND ++$iter <= 8) { // 8, ok 
-				if (isset($trainingDates[$weekday]) AND ($daysLeft > 0 OR $trainingDates[$weekday]['zeit'] >= $nowTime)) {
+			// TODO: check condition
+			while (/*false === $nextTraining AND*/ ++$iter <= 8) { // 8, ok 
+				// TODO: date check for $trainings
+				if (isset($trainingDates[$weekday])
+					AND ($daysLeft > 0 OR $trainingDates[$weekday]['zeit'] >= $nowTime)) {
+					// TODO: should be a Practice object
 					$nextTraining = array(
 						'wtag'    => $trainingDates[$weekday]['tag'],
 						'datum'   => strtotime('+ '.$daysLeft.'days'),
@@ -104,23 +114,27 @@ class Practice {
 						'ort'     => $trainingDates[$weekday]['ort'],
 						'anreise' => $trainingDates[$weekday]['anreise'],
 					);
+
+					// correct timestamp
+					list($ntHour, $ntMin) = explode(':', $nextTraining['zeit']);
+					$ntMin = (int) $ntMin; // kludge: cut the tail
+					$ntDay	= date('d', $nextTraining['datum']);
+					$ntMon	= date('m', $nextTraining['datum']);
+					$ntYear	= date('Y', $nextTraining['datum']);
+					$nextTraining['when'] = mktime($ntHour, $ntMin, 0, $ntMon, $ntDay, $ntYear);
+
+					$nextTraining['practice_id'] = date('Y-m-d H:i:s', $nextTraining['when']);
+
+					self::$next[] = $nextTraining;
 				}
 				$weekday = ++$weekday % 7;
 				++$daysLeft;
 			}
-			// correct timestamp
-			list($ntHour, $ntMin) = explode(':', $nextTraining['zeit']);
-			$ntMin = (int) $ntMin; // kludge: cut the tail
-			$ntDay	= date('d', $nextTraining['datum']);
-			$ntMon	= date('m', $nextTraining['datum']);
-			$ntYear	= date('Y', $nextTraining['datum']);
-			$nextTraining['when'] = mktime($ntHour, $ntMin, 0, $ntMon, $ntDay, $ntYear);
 
-			self::$next = $nextTraining;
 			self::storeNext($cid);
 		}
 
-		return self::$next;
+		return self::$next[0];
 	}
 
 	// TODO: add constructor Practice(int)
@@ -132,11 +146,11 @@ class Practice {
 		if (!$pid) {
 			// find next training p_id
 			// find max PID in DB which is smaller than time() - 120 Min
-			$now = time() + 7 * 24 * 3600;
+			$now = time();
 			$date = new DateTime("@{$now}");
 			$now = $date->format('Y-m-d H:i:s');
-			$q = "SELECT MAX(`practice_id`) AS `pid` FROM `{$table}` "
-				."WHERE `practice_id` < '{$now}' AND `club_id` = '{$this->club_id}' AND `name` = 'RESET'";
+			$q = "SELECT MIN(`practice_id`) AS `pid` FROM `{$table}` "
+				."WHERE `practice_id` >= '{$now}' AND `club_id` = '{$this->club_id}' AND `name` = 'RESET'";
 			$res = DbQuery($q);
 			if (0 == mysql_num_rows($res)) {
 				die('Err0r!');
@@ -188,29 +202,44 @@ class Practice {
 		Das nächste Training ist am <strong><?php print $this->wtag.', '.$this->datum ?>
 		um <?php print $this->begin; ?> Uhr</strong> (Beckenzeit)
 		<hr />
-		<a href="<?php print $plink; ?>">Permalink</a> (<?php print $plink_id; ?>)
+		<a href="<?php print $plink; ?>">Permalink</a> (<?php print $plink_id; ?>)<br />
+		<em>(Fehler: practice_id in Datenbank ist falsche Zeitzone)</em>
 		<?php
+		print '<p>'
+				.'<h3>Kommende Trainings ('.count(self::$next).')</h3>';
+		$this->displayUpcomingList();
+		print '</p>';
 	}
-	function displayNext() {
+	function displayUpcomingList() {
 		// information about the practice
 		// TODO: and NOT about the upcoming one
 		//       (this might coincide, but doesn't have to)
-		$wtag = self::$next['wtag'];
-		$datum = date('d.m.', self::$next['datum']);
-		list($begin, $end) = explode(' - ', self::$next['zeit']);
-		$ort = self::$next['ort'];
-		$plink_id = preg_replace('/[^0-9]/', '', $this->practice_id);
-		$plink = '?p='.$plink_id;
+		print '<ul>';
+		foreach(self::$next as $p) {
+			$wtag = $p['wtag'];
+			$datum = date('d.m.', $p['datum']);
+			list($begin, $end) = explode(' - ', $p['zeit']);
+			$ort = $p['ort'];
+			$plink_id = preg_replace('/[^0-9]/', '', $p['practice_id']);
+			$view_link = '?p='.$plink_id;
+			// TODO: add player name to links
+			$yes_link = $view_link.'&amp;zusage=1';
+			$no_link = $view_link.'&amp;absage=1';
 
-		// information about the players
-		// ...
-		?>
-		Das nächste Training ist am <strong><?php print $wtag.', '.$datum ?>
-		um <?php print $begin; ?> Uhr</strong> (Beckenzeit)
-		im <?php print $ort; ?>
-		<hr />
-		<a href="<?php print $plink; ?>">Permalink</a> (<?php print $plink_id; ?>)
-		<?php
+			// information about the players
+			// ...
+
+			?>
+			<li>Training am <strong><?php print $wtag.', '.$datum ?>
+			um <?php print $begin; ?> Uhr</strong>
+			im <?php print $ort; ?>
+			<a href="<?php print $view_link; ?>">Ansehen</a>
+			| <a href="<?php print $yes_link; ?>">Zusagen</a>
+			| <a href="<?php print $no_link; ?>">Absagen</a>
+			(<?php print $plink_id; ?>)</li>
+			<?php
+		}
+		print '</ul>';
 	}
 }
 ?>
